57fd9ba2d90500dae81ddee3d0e2fa88db55cc0c
Matt Brown <mattbrown@spotify.com>
2025-11-17 13:54:20 -0500

    add argparse, flags for port and name (#6)

--- hello-world/java/src/main/java/com/mattnworb/helloworld/HelloWorldCli.java
+++ hello-world/java/src/main/java/com/mattnworb/helloworld/HelloWorldCli.java
@@ -7,14 +7,40 @@ import com.mattnworb.helloworld.v1.Service.SayHelloResponse;
 import io.grpc.Grpc;
 import io.grpc.InsecureChannelCredentials;
 import io.grpc.ManagedChannel;
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
 
 public class HelloWorldCli {
   public static void main(String[] args) {
-    String target = "localhost:50051";
+
+    ArgumentParser parser =
+        ArgumentParsers.newFor("HelloWorldCli")
+            .build()
+            .defaultHelp(true)
+            .description("Command-line client for HelloWorld gRPC service.");
+
+    parser.addArgument("--target").setDefault("localhost:50051").help("The address to connect to.");
+
+    parser.addArgument("name").help("The name to send to the server.");
+
+    Namespace ns = null;
+    try {
+      ns = parser.parseArgs(args);
+    } catch (ArgumentParserException e) {
+      parser.handleError(e);
+      System.exit(1);
+      return;
+    }
+
+    String target = ns.getString("target");
+    String name = ns.getString("name");
+
     ManagedChannel channel =
         Grpc.newChannelBuilder(target, InsecureChannelCredentials.create()).build();
 
-    SayHelloRequest request = SayHelloRequest.newBuilder().setName("Joe").build();
+    SayHelloRequest request = SayHelloRequest.newBuilder().setName(name).build();
 
     HelloWorldServiceGrpc.HelloWorldServiceBlockingStub stub =
         HelloWorldServiceGrpc.newBlockingStub(channel);
